05b0915cab8ebbc1064e179ac379cac094c56404
taufiqhida <taufiqh2003@gmail.com>
2023-01-26 09:30:27 +0700

    route

---
+++ routes/user.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router
+
+let response = {
+    "code": 200,
+    "message": "Berhasil Mengirim Data",
+    "data":[
+        {
+            "id": 1,
+            "username": "taufiq",
+            "password" : "taufiq123"
+        },
+        {
+            "id": 2,
+            "username": "nisrina",
+            "password" : "nisrina123"
+        }
+    ]
+}
+
+router.get('/', (req, res) => {
+
+})
